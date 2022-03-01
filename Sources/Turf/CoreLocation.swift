eeaca20876fa18c5e783ff08ebee6090cbea3771
Mateusz Szlosek <slozo@me.com>
2022-03-01 18:56:01 +0100

    Add equatable to CLLocationCoordinate2D

--- Sources/Turf/CoreLocation.swift
+++ Sources/Turf/CoreLocation.swift
@@ -255,4 +255,10 @@ extension LocationCoordinate2D {
         return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
     }
 }
+
+extension CLLocationCoordinate2D: Equatable {
+    public static func == (lhs: CLLocationCoordinate2D, rhs: CLLocationCoordinate2D) -> Bool {
+        lhs.longitude == rhs.longitude && lhs.latitude == rhs.latitude
+    }
+}
 #endif
